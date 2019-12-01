09f68f3f6d4928d121f96a261ad2de5d8458b289
jz560 <jzz08111@gmail.com>
2019-12-01 15:56:11 +0000

    user logout

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -30,4 +30,11 @@ class SessionsController extends Controller
 
         return;
     }
+
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', '已成功退出！');
+        return redirect('login');
+    }
 }
