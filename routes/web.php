9afd37d2cf9c64f0fc9081f24e6fdbf7aa8074a0
jz560 <jzz08111@gmail.com>
2020-02-13 09:33:38 +0000

    user_activation

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,10 @@ Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy'
 
 Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
 
+//password resets
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+
 
